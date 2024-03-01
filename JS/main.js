6aa5a200f763799b1bc3a796f04bd2d3152ece67
Sara Oliveira <sara.desider@gmail.com>
2024-03-01 15:13:57 -0300

    aplicação das mascaras e validação do form

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,62 @@
 $(document).ready(function(){
 
-    $('#telefone').mask('(00)00000-0000', {
+    $('#telefone').mask('(00)00000-0000',{
         placeholder:'(__)_____-____'
     });
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder:'123.456.789-01'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '12345-123'
+    });
+
+    $('form').validate({
+        rules: {
+            nomeCompleto: {
+                required:true
+            },
+
+            email: {
+                required:true,
+                email:true
+            },
+
+            telefone: {
+                required: true
+            },
+
+            cep: {
+                required:true
+            },
+
+            endereco: {
+                required: true
+            },
+
+            cpf: {
+                required:true
+            }
+        },
+        messages:{
+            nomeCompleto:'Digite o nome completo para contato',
+            email: 'Digite seu email',
+            telefone: 'Digite seu telefone para contato',
+            cpf: 'Digite seu numero de CPF',
+            cep: 'Digite o CEP de sua rua',
+            endereco: 'Digite o endereço'
+        },
+        submitHandler:function(form){
+            alert('Formulário enviado com sucesso!');
+            console.log(form)
+        },
+        invalidHandler: function(evento, validados){
+            let camposIncorretos = validados.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Faltam ${camposIncorretos} campos a serem preenchidos`)
+            };
+        }
+
+    })
 })
